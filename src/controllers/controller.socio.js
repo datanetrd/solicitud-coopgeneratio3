b120306b40129b5315eaea40d8a96056ba903099
Silentsnake27 <ramiperez71@gmail.com>
2019-09-28 21:07:31 -0400

    pdf implemented but css styles do not work

--- src/controllers/controller.socio.js
+++ src/controllers/controller.socio.js
@@ -1,14 +1,16 @@
 import request from 'request';
-import regeneratorRuntime, { async } from 'regenerator-runtime';
+import regeneratorRuntime, {
+  async
+} from 'regenerator-runtime';
 import puppeteer from 'puppeteer';
 import fs from 'fs-extra';
 import hbs from 'handlebars';
 import path from 'path';
 import moment from 'moment';
-import DataRegister from  '../models/Data_register';
+import DataRegister from '../models/Data_register';
 import nodemailer from 'nodemailer';
 import nuevoSocios from '../models/Nuevos_socios';
-export async function savesocioDB (req,res) {
+export async function savesocioDB(req, res) {
 
   const captcha = req.body['g-recaptcha-response'];
   // Secret Key
@@ -21,10 +23,10 @@ export async function savesocioDB (req,res) {
   //  req.flash("error", "Captcha Failed");
   // }
 
-   // Make Request To VerifyURL
-request(verifyURL, (err, response, body) => {
-  body = JSON.parse(body);
-  console.log(body);
+  // Make Request To VerifyURL
+  request(verifyURL, (err, response, body) => {
+    body = JSON.parse(body);
+    console.log(body);
 
     // // if not successful
     // if(body.success !== undefined && !body.success){
@@ -33,122 +35,177 @@ request(verifyURL, (err, response, body) => {
     // }
 
 
-  // //If Successful
-  // return res.json({"success": true, "msg":"Captcha passed"});
-});
+    // //If Successful
+    // return res.json({"success": true, "msg":"Captcha passed"});
+  });
 
 
- const {nombre,apellido,cedula,estadocivil,direccionresidencial,provincia,telefonos,celular,oficinatrabajo,direcciontrabajo,telefonotrabajo,fax,puesto,sueldo,fechaingresoempresa,email,ahorromensual,certificadoaportacion,valorcertificado,nombre2,apellido2,cedula2,sucursal} = req.body;
- let errors = [];
- //nombre validations
-   if (nombre.lenght <=3) {
-     errors.push({text: 'El Nombre debe de tener más de 3 caracteres'});
-   }
-   if ( typeof nombre !== 'string' && /^[a-z]+$/i.test(nombre)) {
-     errors.push({text: 'El Nombre no puede tener caracteres númericos'});
-   }
-   //nombre2 validations
-   if (nombre2.lenght <=3) {
-    errors.push({text: 'El segundo nombre debe de tener más de 3 caracteres'});
+  const {
+    nombre,
+    apellido,
+    cedula,
+    estadocivil,
+    direccionresidencial,
+    provincia,
+    telefonos,
+    celular,
+    oficinatrabajo,
+    direcciontrabajo,
+    telefonotrabajo,
+    fax,
+    puesto,
+    sueldo,
+    fechaingresoempresa,
+    email,
+    ahorromensual,
+    certificadoaportacion,
+    valorcertificado,
+    nombre2,
+    apellido2,
+    cedula2,
+    sucursal
+  } = req.body;
+  let errors = [];
+  //nombre validations
+  if (nombre.lenght <= 3) {
+    errors.push({
+      text: 'El Nombre debe de tener más de 3 caracteres'
+    });
   }
-  if ( typeof nombre2 !== 'string' && /^[a-z]+$/i.test(nombre2)) {
-    errors.push({text: 'El segundo nombre no puede tener caracteres númericos'});
+  if (typeof nombre !== 'string' && /^[a-z]+$/i.test(nombre)) {
+    errors.push({
+      text: 'El Nombre no puede tener caracteres númericos'
+    });
   }
-   //apellido validations
-   if (apellido.lenght <=3 ) {
-     errors.push({text: 'El Apellido debe de tener más de 3 caracteres'});
-   }
-   if (typeof apellido !== 'string' && /^[a-z]+$/i.test(apellido)) {
-     errors.push({text: 'El Apellido no puede tener caracteres númericos'});
-   }
-   //apellido2 validations
-   if (apellido2.lenght <=3 ) {
-    errors.push({text: 'El segundo apellido debe de tener más de 3 caracteres'});
+  //nombre2 validations
+  if (nombre2.lenght <= 3) {
+    errors.push({
+      text: 'El segundo nombre debe de tener más de 3 caracteres'
+    });
+  }
+  if (typeof nombre2 !== 'string' && /^[a-z]+$/i.test(nombre2)) {
+    errors.push({
+      text: 'El segundo nombre no puede tener caracteres númericos'
+    });
+  }
+  //apellido validations
+  if (apellido.lenght <= 3) {
+    errors.push({
+      text: 'El Apellido debe de tener más de 3 caracteres'
+    });
+  }
+  if (typeof apellido !== 'string' && /^[a-z]+$/i.test(apellido)) {
+    errors.push({
+      text: 'El Apellido no puede tener caracteres númericos'
+    });
+  }
+  //apellido2 validations
+  if (apellido2.lenght <= 3) {
+    errors.push({
+      text: 'El segundo apellido debe de tener más de 3 caracteres'
+    });
   }
   if (typeof apellido2 !== 'string' && /^[a-z]+$/i.test(apellido2)) {
-    errors.push({text: 'El segundo apellido no puede tener caracteres númericos'});
+    errors.push({
+      text: 'El segundo apellido no puede tener caracteres númericos'
+    });
   }
-   //cedula validations
-   if (cedula.lenght <=8) {
-       errors.push({text: 'La Cedula debe tener mas de  8 digitos'});
-   }
-   //estado civil validations
-   if (!estadocivil) {
-       errors.push({text: 'Por favor Seleccione su Estado Civil'});
-   }
-   //dirección residencial validations
-   if (direccionresidencial.length <= 0) {
-     errors.push({text: 'El siguiente campo esta vacio: Dirección Residencial'});
-    }
-    //provincia validations
-    if (provincia.length <= 0) {
-     errors.push({text: 'El siguiente campo esta vacio: Provincia'});
-    }
-    // telefono validations
-    if (telefonos.length < 8) {
-      errors.push({text: 'El Telefono debe de tener por lo menos 8 Digitos'});
-    }
-    
-    //celular validations
-    if (celular.length < 8) {
-      errors.push({text: 'El Celular debe de tener por lo menos 8 Digitos'});
-    }
-   
-    //oficina validations
-    if (oficinatrabajo.length <= 0) {
-      errors.push({text: 'El siguiente campo esta vacio: Oficina de Trabajo'});
-    }
-    //dirección trabajo validations
-    if (direcciontrabajo.length <= 0) {
-      errors.push({text: 'El siguiente campo esta vacio: Dirección de Trabajo'});
-    }
-    // telefono trabajo validations
-    if (telefonotrabajo.length < 8) {
-      errors.push({text: 'El Telefono del Trabajo debe de tener por lo menos 8 Digitos'});
-    }
-    // fecha ingreso empresa validations
-    if (fechaingresoempresa.length <= 0) {
-      errors.push({text: 'El siguiente campo esta vacio: Fecha de Ingreso Empresa'});
-    }
-    //fax validation
-    if (fax.length <= 0) {
-      errors.push({text: 'El siguiente campo esta vacio: Fax'});
-    }
-    //puesto validation
-    if (puesto.length <= 0) {
-      errors.push({text: 'El siguiente campo esta vacio: Puesto'});
-    }
-    //sueldo validation
-    if (sueldo.length <= 0) {
-      errors.push({text: 'El siguiente campo esta vacio: Sueldo'});
-    }
-    //ahorro validation
-    if (ahorromensual.length <= 0) {
-      errors.push({text: 'El siguiente campo esta vacio: ahorromensual'});
-    }
-    
-    
-    //errors validations 
-    if(errors.length > 0) {
-      res.render('form', {errors,nombre,apellido,cedula,direccionresidencial,telefonos,celular,oficinatrabajo,direcciontrabajo,telefonotrabajo,fax,puesto,fechaingresoempresa,sueldo,email,ahorromensual,certificadoaportacion,valorcertificado,nombre2,apellido2,cedula2});
-    } else {
+  //cedula validations
+  if (cedula.lenght <= 8) {
+    errors.push({
+      text: 'La Cedula debe tener mas de  8 digitos'
+    });
+  }
+  //estado civil validations
+  if (!estadocivil) {
+    errors.push({
+      text: 'Por favor Seleccione su Estado Civil'
+    });
+  }
+  //dirección residencial validations
+  if (direccionresidencial.length <= 0) {
+    errors.push({
+      text: 'El siguiente campo esta vacio: Dirección Residencial'
+    });
+  }
+  //provincia validations
+  if (provincia.length <= 0) {
+    errors.push({
+      text: 'El siguiente campo esta vacio: Provincia'
+    });
+  }
+  // telefono validations
+  if (telefonos.length < 8) {
+    errors.push({
+      text: 'El Telefono debe de tener por lo menos 8 Digitos'
+    });
+  }
 
-      
-      try {
-        let dataName = await nuevoSocios.create({
+  //celular validations
+  if (celular.length < 8) {
+    errors.push({
+      text: 'El Celular debe de tener por lo menos 8 Digitos'
+    });
+  }
 
-      nombre,
-      apellido
-        });
+  //oficina validations
+  if (oficinatrabajo.length <= 0) {
+    errors.push({
+      text: 'El siguiente campo esta vacio: Oficina de Trabajo'
+    });
+  }
+  //dirección trabajo validations
+  if (direcciontrabajo.length <= 0) {
+    errors.push({
+      text: 'El siguiente campo esta vacio: Dirección de Trabajo'
+    });
+  }
+  // telefono trabajo validations
+  if (telefonotrabajo.length < 8) {
+    errors.push({
+      text: 'El Telefono del Trabajo debe de tener por lo menos 8 Digitos'
+    });
+  }
+  // fecha ingreso empresa validations
+  if (fechaingresoempresa.length <= 0) {
+    errors.push({
+      text: 'El siguiente campo esta vacio: Fecha de Ingreso Empresa'
+    });
+  }
+  //fax validation
+  if (fax.length <= 0) {
+    errors.push({
+      text: 'El siguiente campo esta vacio: Fax'
+    });
+  }
+  //puesto validation
+  if (puesto.length <= 0) {
+    errors.push({
+      text: 'El siguiente campo esta vacio: Puesto'
+    });
+  }
+  //sueldo validation
+  if (sueldo.length <= 0) {
+    errors.push({
+      text: 'El siguiente campo esta vacio: Sueldo'
+    });
+  }
+  //ahorro validation
+  if (ahorromensual.length <= 0) {
+    errors.push({
+      text: 'El siguiente campo esta vacio: ahorromensual'
+    });
+  }
 
-        let data = await DataRegister.create({
 
+  //errors validations 
+  if (errors.length > 0) {
+    res.render('form', {
+      errors,
       nombre,
       apellido,
       cedula,
-      estadocivil,
       direccionresidencial,
-      provincia,
       telefonos,
       celular,
       oficinatrabajo,
@@ -156,8 +213,8 @@ request(verifyURL, (err, response, body) => {
       telefonotrabajo,
       fax,
       puesto,
-      sueldo,
       fechaingresoempresa,
+      sueldo,
       email,
       ahorromensual,
       certificadoaportacion,
@@ -165,98 +222,147 @@ request(verifyURL, (err, response, body) => {
       nombre2,
       apellido2,
       cedula2
-
-        });
-
-    
-        if (data) {
-            
-          res.redirect('/');
-        }
-    
-      } catch (e) {
-          console.log(e);
-          res.status(500).json({
-              message: 'Algo ha ido Mal',
-          data: {}
-        });
-        // res.redirect('/');
-      };
-      const datta = req.body;
-      const compile = async function (templateName,datta){
-        const filePath = path.join(process.cwd(), './src/views', `${templateName}.hbs`);
-        const html = await fs.readFileSync(filePath, 'utf-8');
-        console.log(html);
-        return hbs.compile(html)(datta);
-
-      };
-      ( async function (){
-        
-        try {
-          const browser = await puppeteer.launch();
-          const page = await browser.newPage();
-
-          const content = await compile('pdf27',datta);
-          console.log(content);
-
-          await page.goto(`data:text/html,${content}`, { waitUntil: 'networkidle0' });
-          await page.emulateMedia('screen');
-          await page.pdf({
-            path: `${nombre}.pdf`,
-            format: 'A4',
-            printBackground: true
-          });
-          console.log('done');
-          await browser.close();
-          
-  
-        } catch (e) {
-          console.log('ha habido un error', e);
-        }
-  
-        })();
-      
-      const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            //se indica el usuario y password
-            user: 'ramiperez71@gmail.com',
-            pass: 'Ramesh222'
-        }
     });
-if (sucursal === "santo domingo") {
-     //Opciones para el Envio del correo
-const mailOptions = {
-  from: 'ramiperez71@gmail.com',
-  to: 'poner correo here',
-  subject: 'nueva solicitud socio',
-};
+  } else {
 
-//Envio del mail
-transporter.sendMail(mailOptions, function(error,info){
-  //validar que haya habido un error
-  if (error) {
-    console.log(error);
-  }
-})
-}
-if (sucursal === "santiago") {
+
+    try {
+      let dataName = await nuevoSocios.create({
+
+        nombre,
+        apellido
+      });
+
+      let data = await DataRegister.create({
+
+        nombre,
+        apellido,
+        cedula,
+        estadocivil,
+        direccionresidencial,
+        provincia,
+        telefonos,
+        celular,
+        oficinatrabajo,
+        direcciontrabajo,
+        telefonotrabajo,
+        fax,
+        puesto,
+        sueldo,
+        fechaingresoempresa,
+        email,
+        ahorromensual,
+        certificadoaportacion,
+        valorcertificado,
+        nombre2,
+        apellido2,
+        cedula2
+
+      });
+
+
+      if (data) {
+
+        res.redirect('/');
+      }
+
+    } catch (e) {
+      console.log(e);
+      res.status(500).json({
+        message: 'Algo ha ido Mal',
+        data: {}
+      });
+      // res.redirect('/');
+    };
+    const datta = req.body;
+    const compile = async function (templateName, datta) {
+      const filePath = path.join(process.cwd(), './src/views', `${templateName}.hbs`);
+      const html = await fs.readFileSync(filePath, 'utf-8');
+      console.log(html);
+      return hbs.compile(html)(datta);
+
+    };
+    (async function () {
+
+      try {
+        const browser = await puppeteer.launch({
+          args: ['--no-sandbox'],
+          headless: true
+        });
+        const page = await browser.newPage();
+
+        const content = await compile('pdf27', datta);
+        console.log(content);
+
+        await page.goto(`data:text/html;charset=UTF-8,${content}`, {
+          waitUntil: 'networkidle0'
+        });
+        const options = {
+          width: '1230px',
+          headerTemplate: "<p></p>",
+          footerTemplate: "<p></p>",
+          displayHeaderFooter: false,
+          margin: {
+            top: "10px",
+            bottom: "30px"
+          },
+          printBackground: true,
+          format: 'A4',
+          path: `${nombre}.pdf`
+        }
+        await page.emulateMedia('screen');
+        await page.pdf(options);
+        console.log('done');
+        await browser.close();
+
+
+      } catch (e) {
+        console.log('ha habido un error', e);
+      }
+
+    })();
+
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        //se indica el usuario y password
+        user: 'ramiperez71@gmail.com',
+        pass: 'Ramesh222'
+      }
+    });
+    if (sucursal === "santo domingo") {
       //Opciones para el Envio del correo
-const mailOptions = {
-  from: 'ramiperez71@gmail.com',
-  to: 'poner correo here',
-  subject: 'nueva solicitud socio',
-};
+      const mailOptions = {
+        from: 'ramiperez71@gmail.com',
+        to: 'poner correo here',
+        subject: 'nueva solicitud socio',
+      };
 
-//Envio del mail
-transporter.sendMail(mailOptions, function(error,info){
-  //validar que haya habido un error
-  if (error) {
-    console.log(error);
-  }
-})
-}
- 
+      //Envio del mail
+      transporter.sendMail(mailOptions, function (error, info) {
+        //validar que haya habido un error
+        if (error) {
+          console.log(error);
+        }
+      })
+    }
+    if (sucursal === "santiago") {
+      //Opciones para el Envio del correo
+      const mailOptions = {
+        from: 'ramiperez71@gmail.com',
+        to: 'poner correo here',
+        subject: 'nueva solicitud socio',
+      };
+
+      //Envio del mail
+      transporter.sendMail(mailOptions, function (error, info) {
+        //validar que haya habido un error
+        if (error) {
+          console.log(error);
+        }
+      })
+    }
+
   }
 };
 //     // fields: ['nombre', 'cedula', 'estadocivil', 'direccionresidencial', 'provincia', 'telefonos', 'celular', 'oficinatrabajo', 'direcciontrabajo', 'telefono', 'fax', 'puestotrabajo', 'fechaingresoempresa', 'sueldo', 'email']
