f0375813a71fac54119632af12508b911f5d7f56
“silentsnake27” <ramiperez71@gmai.com>
2019-11-17 17:12:42 -0400

    solicitud socios v1.0

--- dist/controllers/Form.Controller.js
+++ dist/controllers/Form.Controller.js
@@ -300,7 +300,7 @@ async function savesocioDB(req, res, next) {
             // width: '816px', //version para dev
             height: '1205px',
             //hay que editar esto bien para heroku
-            width: '920px',
+            width: '930px',
             //hay que editar esto bien para heroku
             // format: "A4",
             headerTemplate: "<p></p>",
