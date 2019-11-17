3aadf5789c8d67105a7677d0b217e371c55fcab5
“silentsnake27” <ramiperez71@gmai.com>
2019-11-17 16:23:26 -0400

    solicitud socios v1.0

--- dist/controllers/Form.Controller.js
+++ dist/controllers/Form.Controller.js
@@ -298,9 +298,9 @@ async function savesocioDB(req, res, next) {
           const options = {
             // height: '1110px', // version para dev
             // width: '816px', //version para dev
-            height: '1240px',
+            height: '1220px',
             //hay que editar esto bien para heroku
-            width: '1000px',
+            width: '980px',
             //hay que editar esto bien para heroku
             // format: "A4",
             headerTemplate: "<p></p>",
--- src/controllers/Form.Controller.js
+++ src/controllers/Form.Controller.js
@@ -273,8 +273,8 @@ export async function savesocioDB(req, res,next) {
           const options = {
             // height: '1110px', // version para dev
             // width: '816px', //version para dev
-            height: '1240px', //hay que editar esto bien para heroku
-            width: '1000px', //hay que editar esto bien para heroku
+            height: '1220px', //hay que editar esto bien para heroku
+            width: '980px', //hay que editar esto bien para heroku
             // format: "A4",
             headerTemplate: "<p></p>",
             footerTemplate: "<p></p>",
